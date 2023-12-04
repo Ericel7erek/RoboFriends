d8b38efe64d4cb938e033ef307ec5fb85e4ecc5b
Eric <ericmalah@Erics-MacBook-Pro.local>
2023-12-04 15:36:22 +0100

    New

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../Components/CardList';
 // import { robots } from './robots';
 import SearchBox from '../Components/SearchBox';
@@ -6,36 +6,49 @@ import'./App.css'
 import Scroll from '../Components/Scroll'
 import ErrorBoundry from '../Components/ErrorBoundry';
 
-class App extends Component {
-  constructor() {
-    super()
-      this.state = {
-      robots: [],
-      searchfield: '',
-  }
-}
-componentDidMount() {
+function App(){
+//   constructor() {
+//     super()
+//       this.state = {
+//       robots: [],
+//       searchfield: '',
+//   }
+// }
+// componentDidMount() {
+//   fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(response=>response.json())
+//   .then(users=> this.setState({ robots: users }))}
+const [robots, setRobots] = useState([])
+const [searchfield, setSearchfield] = useState('')
+
+useEffect(()=>{
+
   fetch('https://jsonplaceholder.typicode.com/users')
   .then(response=>response.json())
-  .then(users=> this.setState({ robots: users }))}
+  .then(users=> {setRobots( users )})
 
-OnSearchChange = (event) => {
-  this.setState({ searchfield: event.target.value })
+},[])
+
+const OnSearchChange = (event) => {
+  setSearchfield(event.target.value )
 }
-  render(){
-    const { robots, searchfield } = this.state
+
+    
     const filteredRobots = robots.filter(robot=>{
       return robot.name.toLowerCase().includes(searchfield.toLowerCase())
     })
+    
     return (!robots.length)?
       
         <h1 className='tc'>Loading</h1> :
       
      
   (
+    
     <div className='tc'>
       <h1 className='f1'>RoboFriends</h1>
-      <SearchBox searchChange={this.OnSearchChange}/>
+      
+      <SearchBox searchChange={OnSearchChange}/>
       <Scroll>
         <ErrorBoundry>
       <CardList robots = {filteredRobots} />
@@ -45,5 +58,6 @@ OnSearchChange = (event) => {
     );
     
   }
-}
+
+
 export default App;
